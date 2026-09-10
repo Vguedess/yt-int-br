4206009e06109c4da58d5b365ee9e596d83983a8
Victor Guedes Sant'Ana <69536518+Vguedess@users.noreply.github.com>
2026-09-10 15:32:57 +0000

    feat: add ComplexSearch page

---
+++ app/complex-search/page.tsx
@@ -0,0 +1,7 @@
+import { ComplexSearchClient } from '@/app/components/ComplexSearchClient';
+
+export const dynamic = 'force-dynamic';
+
+export default function ComplexSearchPage() {
+  return <ComplexSearchClient />;
+}
